cdb1627ddd62934d36c2793e1c1a5c85583686a4
nhruo <nhruo123@gmail.com>
2020-03-14 13:55:43 -0700

    keept eax and ebx in _start

--- os/start/start.asm
+++ os/start/start.asm
@@ -29,10 +29,10 @@ resb 4096
 section .data
 gdt:
 
- gdt_null:
+ .gdt_null:
  dq 0
 
- gdt_code:
+ .gdt_code:
  dw 0xFFFF ; limit
  dw 0x0 ; base
  db 0 ; base
@@ -40,23 +40,23 @@ gdt:
  db 11001111b
  db 0
 
- gdt_data:
+ .gdt_data:
  dw 0xFFFF
  dw 0x0
  db 0
  db 10010010b
  db 11001111b
  db 0 
-gdt_end:
+.gdt_end:
 
 
 gdt_desc:
- dw gdt_end - gdt - 1
+ dw gdt.gdt_end - gdt - 1
  dd gdt
  
 
 section .text
-global _start:function (_start_end - _start)
+global _start:function (_start.end - _start)
 _start:
 
 	extern _kernel_start
@@ -67,25 +67,25 @@ _start:
 	mov esi, 0 ; first address to map is 0
 	mov ecx, 1023 ; we will map only 1023 the last one will be the vga buffer
 
-	_map_loop:
+._map_loop:
 	cmp esi, _kernel_start - 0xC0000000 ; we will skip all adresses that are not in the kernel
-	jl _point_to_next_page
+	jl ._point_to_next_page
 
 	cmp esi, _kernel_end - 0xC0000000  ; we are done mapping the kernel
-	jge _map_vga_buffer
+	jge ._map_vga_buffer
 
 	mov edx, esi ; get the physical address of the page
 	or edx, 0x003 ; set w/ring0/present
 	mov [edi], edx ; write it to table 
 
-_point_to_next_page:
+._point_to_next_page:
 
 
 	add esi, 4096 ; add page size to index (page is 4096 bytes)
 	add edi , 4 ; add the size of the last entry to the pointer of the current page
-	loop _map_loop
+	loop ._map_loop
 
-_map_vga_buffer:
+._map_vga_buffer:
 	mov dword [boot_page_table1 - 0xC0000000 + 1023 * 4], 0x000B8007 ; map VGA to 0XC03FF000
 
 	mov dword [boot_page_directory - 0xC0000000 + 0 * 4], boot_page_table1 - 0xC0000000 + 0x003  ; identity map the kernel 
@@ -95,7 +95,7 @@ _map_vga_buffer:
 	mov ecx, boot_page_directory - 0xC0000000
 	mov cr3, ecx
 
-	lea eax, [_higher_half]
+	
 
 	; Enable paging and the write-protect bit.
 	mov ecx, cr0
@@ -104,13 +104,13 @@ _map_vga_buffer:
 
 	; Jump to higher half with an absolute jump. 
 	
+	lea ecx, [_higher_half]
+	jmp ecx
 
-	jmp eax
-
-_start_end:
+.end:
 
 
-global _higher_half:function
+global _higher_half:function (_higher_half.end - _higher_half)
 _higher_half:
 
 	mov dword [boot_page_directory + 0], 0
@@ -121,15 +121,15 @@ _higher_half:
 
 	lgdt [gdt_desc]
 	
-	jmp 0x08:(flush_cs)
+	jmp 0x08:(.flush_cs)
 	
-    flush_cs:
-    mov ax, 0x10
-	mov ds, ax
-	mov ss, ax
-	mov es, ax
-	mov gs, ax
-	mov fs, ax
+    .flush_cs:
+    mov cx, 0x10
+	mov ds, cx
+	mov ss, cx
+	mov es, cx
+	mov gs, cx
+	mov fs, cx
 
 	; Set up the stack.
 	mov esp, stack_top
@@ -140,3 +140,4 @@ _higher_half:
 	cli
 .hang:	hlt
 	jmp .hang
+.end:
